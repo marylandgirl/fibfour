a16e2de617a92f618e5f67d8e11d3ca6aae407fb
Kim Levin <levin.kimberly@gmail.com>
2018-01-29 10:28:17 -0500

    Initial commit

---
+++ src/main/java/com/example/fibfour/FibfourApplication.java
@@ -0,0 +1,12 @@
+package com.example.fibfour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibfourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibfourApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/fibfour/MainController.java
@@ -0,0 +1,38 @@
+package com.example.fibfour;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String printFibSums(@RequestParam int number) {
+
+        int n1 = number;
+        int n2 = number + 1;
+        int n3 = number + 2;
+        int n4 = number + 3;
+        int n5;
+
+        int count = 20;
+
+        System.out.print(n1 + " " + n2 + " " + n3 + " "  +n4 + " ");
+        for(int i=4;i<count;++i)
+        {
+
+            n5 = n1 + n2 + n3 + n4;
+            n1=n2;
+            n2=n3;
+            n3=n4;
+            n4=n5;
+            System.out.print(" "+n5);
+        }
+
+//        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4);
+
+        return "";
+
+    }
+}
